a26b0113ff01f20acf8c2528a6ee9a0b69fc4cf6
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-05-11 20:20:07 +0900

    useStateとuseCallback使い方を学習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
-import styles from 'src/styles/Home.module.css'
-import Head from 'next/head'
-import { Footer } from 'src/components/Footer'
-import { Header } from 'src/components/Header'
-import { Main } from 'src/components/Main'
-import { useCallback, useEffect, useState } from 'react';
+import styles from "src/styles/Home.module.css";
+import Head from "next/head";
+import { Footer } from "src/components/Footer";
+import { Header } from "src/components/Header";
+import { Main } from "src/components/Main";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   // useCallback(コールバック関数, 依存配列); 再レンダリングを防ぐ
   const handleClick = useCallback(() => {
@@ -20,15 +22,27 @@ export default function Home() {
     // setFoo(count + 1);
   }, [count]);
 
-    // ライフサイクル マウント時、アンマウント時に走る
-    useEffect(() => {
-      // マウント時に走る
-      document.body.style.backgroundColor = "lightblue";
-      return () => {
-        // アンマウント時に走る
-        document.body.style.backgroundColor = "";
-      }
-    }, []);
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 6) {
+      alert("制限文字数以内にしようぜ");
+      return;
+    }
+    setText(e.target.value);
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  // ライフサイクル マウント時、アンマウント時に走る
+  useEffect(() => {
+    // マウント時に走る
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      // アンマウント時に走る
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -36,12 +50,12 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
-      <button
-        onClick={handleClick}
-        >ボタン</button>
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main root="index" />
       <Footer />
     </div>
-  )
+  );
 }
