64ca9b17e7f841ff39a21a7fe576480ae095059e
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-05 08:02:03 +0900

    jsonplaceholder api からデータフェッチ

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,37 @@
 import styles from "src/styles/Home.module.css";
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
+import { useCallback, useEffect, useState } from "react";
 
 // props 展開せずにその場その場でpropsから利用した方がpropsから渡ってきていることがわかりやすい説
 const Home = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts()]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-
-      {props.isShow ? <h1>{props.count}</h1> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>{props.isShow ? "非表示" : "表示"}</button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加する</button>
-      <ul>
-          {props.array.map(item => {
-              return (
-                  <li key={item}>{item}</li>
-              )
+      {posts.length > 0 ? (
+        <ol>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
           })}
-      </ul>
-
-      <Main root="index" />
-      <Footer />
+        </ol>
+      ) : null}
     </div>
   );
-}
+};
 
 export default Home;
