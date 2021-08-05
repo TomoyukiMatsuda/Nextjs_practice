4738cbe2bfa95c4deff4c4e622eec6972da2ff24
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-05 09:03:20 +0900

    APIフェッチのローティング、エラーハンドリング

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useState } from "react";
+
+export const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getPosts = useCallback(async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!res.ok) {
+        throw new Error("データ取得中にエラーが発生、データ取得失敗");
+      }
+      const json = await res.json();
+      setPosts(json);
+    } catch (e) {
+      setError(e);
+    }
+    // 最終的には成功でも失敗でも Loading はfalseとなる
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts()]);
+
+  if (loading) {
+    return <div>ローディング中です。</div>;
+  }
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+  if (posts.length === 0) {
+    return <div>データはからです。</div>;
+  }
+
+  return (
+    <ol>
+      {posts.map((post) => {
+        return <li key={post.id}>{post.title}</li>;
+      })}
+    </ol>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,35 +1,17 @@
 import styles from "src/styles/Home.module.css";
 import Head from "next/head";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { Posts } from "../components/Posts";
 
 // props 展開せずにその場その場でpropsから利用した方がpropsから渡ってきていることがわかりやすい説
-const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts()]);
-
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </div>
   );
 };
