9ca2ce3f66904a6816184223937229073015e57a
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-28 08:02:44 +0900

    ページ / -> posts に変更

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,7 +1,7 @@
 import styles from "src/styles/Home.module.css";
 import Head from "next/head";
 import { Header } from "src/components/Header";
-import { Posts } from "../components/Posts";
+import { Posts } from "../../components/Posts";
 
 // props 展開せずにその場その場でpropsから利用した方がpropsから渡ってきていることがわかりやすい説
 const Home = () => {
