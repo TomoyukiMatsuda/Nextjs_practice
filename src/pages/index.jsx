71a05935d9360a04acf9d23f03f9462f6c4262da
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-28 08:12:46 +0900

    Indexページをリファクタリング

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,16 @@
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
-const Index = (props) => {
+const Index = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>About Page</title>
-      </Head>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加する</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Main root="about" />
-      <Footer />
+      <h1>Next.jsで学ぶ React 講座</h1>
+      <p>JSONPlaceholder のAPIを色々叩くよ！</p>
     </div>
   );
 };
