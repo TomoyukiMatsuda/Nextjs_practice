f3f3186d63be32bd89a800eff4e5bfb4a1f464dd
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-28 08:17:05 +0900

    不必要なコードをリファクタリング

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,21 +1,15 @@
 import Head from "next/head";
 import "src/styles/globals.css";
-import { useCounter } from "../hooks/useCounter";
-import { useInputArr } from "../hooks/useInputArr";
-import { useBgColor } from "../hooks/useBgColor";
 
 const MyApp = ({ Component, pageProps }) => {
   // ページ間で State(状態) を共通管理 _app.jsx にリフトアップしている。これはReactにあるものではなく、Next.js だからできること
-  const counter = useCounter();
-  const inputArr = useInputArr();
-  useBgColor();
 
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArr} />
+      <Component {...pageProps} />
     </>
   );
 };
