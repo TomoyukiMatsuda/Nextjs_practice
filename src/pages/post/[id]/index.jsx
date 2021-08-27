1fc51e62d90eb464425e12305c043aa6013dcd3d
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-08-27 20:55:56 +0900

    post詳細フェッチ処理

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -2,19 +2,28 @@ import styles from "src/styles/Home.module.css";
 import Head from "next/head";
 import { Header } from "src/components/Header";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 // props 展開せずにその場その場でpropsから利用した方がpropsから渡ってきていることがわかりやすい説
 const PostId = () => {
   const router = useRouter();
+  // query.id が取得できたらAPIを叩くようにする（query.id が存在しない状態でApiを叩くことを防いでいる）
+  const { data, error } = useSWR(
+    router.query.id
+      ? `https://jsonplaceholder.typicode.com/posts/${router.query.id}`
+      : null
+  );
+
+  console.log({ data, error });
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Index Page</title>
+        <title>{data?.title}</title>
       </Head>
       <Header />
-      {/*id という名前は ディレクトリ名[id]に対応している*/}
-      <div>{router.query.id}</div>
+      <h1>{data?.title}</h1>
+      <div>{data?.body}</div>
     </div>
   );
 };
